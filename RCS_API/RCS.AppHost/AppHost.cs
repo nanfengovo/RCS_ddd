f2eb4f42f9ac258c5d5814b91e1d257a27314209
nanfeng <nanfengovo@gmail.com>
2026-05-29 14:34:37 +0800

    chore(init): 初始化全栈工程与DevOps基建

---
+++ RCS_API/RCS.AppHost/AppHost.cs
@@ -0,0 +1,28 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+//1. 引入业务关系型数据库（PostgreSQL) + PgAdmin可视化工具
+var postgres = builder.AddPostgres("postgres-server")
+                                                                .WithPgAdmin()
+                                                                .AddDatabase("RcsCoreDb");
+
+// 2. 高频状态缓存 (Redis) + RedisInsight 可视化工具
+var redis = builder.AddRedis("redis-cache")
+                   .WithRedisInsight();
+
+// 3. 非结构化/日志/AI Prompt 存储 (MongoDB) + MongoExpress 可视化工具
+var mongo = builder.AddMongoDB("mongodb-server")
+                   .WithMongoExpress()
+                   .AddDatabase("RcsLogDb");
+
+// 4. 事件总线与死信队列 (RabbitMQ) + 自带的 Management UI
+var rabbitmq = builder.AddRabbitMQ("event-bus")
+                      .WithManagementPlugin();
+
+// 5. 注册你的 Web API，并把四大金刚的连接字符串自动注入进去！
+var apiService = builder.AddProject<Projects.RCS_Api>("rcs-api")
+                        .WithReference(postgres)
+                        .WithReference(redis)
+                        .WithReference(mongo)
+                        .WithReference(rabbitmq);
+
+builder.Build().Run();
---
+++ RCS_API/RCS.Application/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace RCS.Application;
+
+public class Class1
+{
+
+}
